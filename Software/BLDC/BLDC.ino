a5941824cce6757907fafc369c354be0fe17bedc
Ariamelon <ariatsoaria@gmail.com>
2024-03-22 15:12:36 +0000

    Tentative code update to V1.1.

--- Software/BLDC/BLDC.ino
+++ Software/BLDC/BLDC.ino
@@ -1,26 +1,28 @@
 #define PWM_MAX_DUTY      254
 #define PWM_MIN_DUTY      32
-#define PWM_START_DUTY    50
+#define PWM_START_DUTY    100
 #define DEBOUNCE_PERIOD   5
-#define BTN_SPEED_UP      PD3
-#define BTN_SPEED_DOWN    PD2
 
 void pwm_duty_set(uint8_t duty);
-void bldc_comm();
+void bldc_comm_state(void);
+void bldc_comm_startup(void);
+void bldc_comm_shutdown(void);
  
 uint8_t bldc_state = 0, motor_speed;
 uint16_t i;
 bool motor_direction = 1;
 
 void setup() {
-  // Enable input with pullups for the speed control buttons.
-  pinMode(BTN_SPEED_UP, INPUT_PULLUP);
-  pinMode(BTN_SPEED_DOWN, INPUT_PULLUP);
+  Serial.begin(9600);
 
   DDRB  |= 0b00001110; // Configure HI_A (PB3), HI_B (PB2) and HI_C (PB1) (high side) as outputs.
+  PORTB |= 0b00110000; // Enable speed+ (PB5) and speed- (PB4) pullups.
   PORTB &= 0b11110001; // Disable HI_A (PB3), HI_B (PB2) and HI_C (PB1).
   DDRC  |= 0b00000111; // Configure LI_A (PC2), LI_B (PC1) and LI_C (PC0) (low side) as outputs.
   PORTC &= 0b11111000; // Disable LI_A (PC2), LI_B (PC1) and LI_C (PC0).
+  DDRD  |= 0b00011100; // Configure red LED (PD2), yellow LED (PD3) and green LED (PD4) as outputs.
+  PORTD &= 0b11100011; // Disable red LED (PD2), yellow LED (PD3) and green LED (PD4).
+  PORTD |= 0b00000100; // Enable red LED (PD2).
 
   // Initialize timers 1 and 2.
   TCCR1A = 0;
@@ -31,35 +33,39 @@ void setup() {
   TCCR2B = 0b00000001;
 
   ACSR   = 0b00010000; // Disable and clear ACI flag (analog comparator interrupt).
-
-  // Setup starting PWM with duty cycle = PWM_START_DUTY.
-  motor_speed = PWM_START_DUTY;
-  pwm_duty_set(motor_speed);
-  
-  // Initial open loop commutation to enable motor to start properly.
-  i = 5000;
-  while(i > 100) {
-    delayMicroseconds(i);
-    bldc_comm();
-    i = i - 20;
-  }
-
   ADCSRA = 0;          // Disable ADC.
-  ADCSRB = 0b01000000; // Enable analog comparator mux.
-  ACSR  |= 0b00001000; // Enable analog comparator interrupt.
+  
+  bldc_comm_shutdown();
 }
 
 void loop() {
   // Speed control via on-board buttons.
-  while(!(digitalRead(BTN_SPEED_UP)) && motor_speed < PWM_MAX_DUTY){
-    motor_speed++;
-    pwm_duty_set(motor_speed);
-    delay(10);
+  while(!(PINB & (1 << PINB5))){
+    if ((motor_speed < PWM_MAX_DUTY) && (motor_speed >= PWM_MIN_DUTY)){
+      motor_speed++;
+      pwm_duty_set(motor_speed);
+    }
+    else if (motor_speed < PWM_MIN_DUTY){
+      Serial.println("Starting up motor.");
+      bldc_comm_startup();
+      Serial.println("Start-up complete.");
+    }
+    Serial.print("Motor speed: ");
+    Serial.println(motor_speed);
+    delay(100);
   }
-  while(!(digitalRead(BTN_SPEED_DOWN)) && motor_speed > PWM_MIN_DUTY){
-    motor_speed--;
-    pwm_duty_set(motor_speed);
-    delay(10);
+  while(!(PINB & (1 << PINB4))){
+    if (motor_speed > PWM_MIN_DUTY){
+      motor_speed--;
+      pwm_duty_set(motor_speed);
+    }
+    else if ((motor_speed <= PWM_MIN_DUTY) && (motor_speed > 0)){
+      Serial.println("Shutting down motor.");
+      bldc_comm_shutdown();
+    }
+    Serial.print("Motor speed: ");
+    Serial.println(motor_speed);
+    delay(100);
   }
 }
 
@@ -74,7 +80,44 @@ void pwm_duty_set(uint8_t duty){
   OCR1A = OCR1B = OCR2A = duty; // Set PWM duty cycle for all outputs.
 }
 
-void bldc_comm(){ // BLDC motor commutation function.
+void bldc_comm_startup(void){
+  // Setup starting PWM with duty cycle = PWM_START_DUTY.
+  motor_speed = PWM_START_DUTY;
+  pwm_duty_set(motor_speed);
+
+  bldc_state = 0;     // Enable BLDC commutation.
+
+  // Initial open loop commutation to enable motor to start properly.
+  i = 5000;
+  while(i > 100) {
+    bldc_comm_state();
+    delayMicroseconds(i);
+    i = i - 20;
+  }
+
+  ADCSRB = 0b01000000; // Enable analog comparator mux.
+  ACSR  |= 0b00001000; // Enable analog comparator interrupt.
+
+  PORTD &= 0b11110111; // Disable yellow LED (PD3).
+  PORTD |= 0b00010000; // Enable green LED (PD4).
+}
+
+void bldc_comm_shutdown(void){
+  // Turn off PWM.
+  motor_speed = 0;
+  pwm_duty_set(motor_speed);
+
+  bldc_state = 6;      // Shutdown BLDC commutation.
+  bldc_comm_state();
+
+  ADCSRB = 0b00000000; // Disable analog comparator mux.
+  ACSR  &= 0b11110111; // Disable analog comparator interrupt.
+
+  PORTD &= 0b11101111; // Disable green LED (PD4).
+  PORTD |= 0b00001000; // Enable yellow LED (PD3).
+}
+
+void bldc_comm_state(void){ // BLDC motor commutation function.
   switch(bldc_state){
     case 0:
       PORTC &= 0b11111010; // Disable LI_A (PC2) and LI_C (PC0).
@@ -136,6 +179,11 @@ void bldc_comm(){ // BLDC motor commutation function.
       ADMUX  = 0b00000101; // Select ADC5 (PC5) as comparator negative input.
       ACSR  &= 0b11111110; // Set interrupt on falling edge.
       break;
+    case 6:
+      // Shut down motor controller.
+      PORTB &= 0b11110001; // Disable HI_A (PB3), HI_B (PB2) and HI_C (PB1).
+      PORTC &= 0b11111000; // Disable LI_A (PC2), LI_B (PC1) and LI_C (PC0).
+      return;
   }
   bldc_state ++; // Advance commutation state by one.
   bldc_state %= 6;
@@ -153,5 +201,5 @@ ISR (ANALOG_COMP_vect) {
     }
   }
 
-  bldc_comm();
+  bldc_comm_state();
 }
