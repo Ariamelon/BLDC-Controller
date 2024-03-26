19de88d13684b49f2faf204f7814e3dcf8147be9
Aria Tso <ariatsoaria@gmail.com>
2024-03-26 12:10:16 +0000

    Update BLDC.ino

--- Software/BLDC/BLDC.ino
+++ Software/BLDC/BLDC.ino
@@ -1,7 +1,7 @@
 #define PWM_MAX_DUTY      255
 #define PWM_MIN_DUTY      32
 #define PWM_START_DUTY    50
-#define DEBOUNCE_PERIOD   1
+#define DEBOUNCE_PERIOD   10
 
 void pwm_duty_set(uint8_t duty);
 void bldc_comm_state(void);
@@ -19,12 +19,12 @@ void setup() {
   DDRD  |= 0b00011100; // Configure red LED (PD2), yellow LED (PD3) and green LED (PD4) as outputs.
   PORTD &= 0b11100111; // Disable yellow LED (PD3) and green LED (PD4).
   PORTD |= 0b00000100; // Enable red LED (PD2).
-  // Initialize timers 1 and 2.
-  TCCR1A = 0;
-  TCCR2A = 0;
-  // Set clock source to clkIO / 1 (no prescaling) for maximum PWM frequency.
-  TCCR1B = 0b00000001;
+
+  TCCR1A = 0;          // Initialize timers 1 and 2.
+  TCCR1B = 0b00000001; // Clock source set to clkIO / 1 (no prescaling).
+  TCCR2A = 0;          // Allows for highest PWM frequency.
   TCCR2B = 0b00000001;
+
   ACSR   = 0b00010000; // Disable and clear ACI flag (analog comparator interrupt).
   ADCSRA = 0;          // Disable ADC.
   
@@ -56,9 +56,7 @@ void loop() {
 }
 
 void pwm_duty_set(uint8_t duty){
-  OCR1A = duty; // Set PWM duty cycle for all outputs.
-  OCR1B = duty;
-  OCR2A = duty;
+  OCR1A = OCR1B = OCR2A = duty; // Set PWM duty cycle for all outputs.
 }
 
 void bldc_comm_startup(void){
@@ -77,7 +75,8 @@ void bldc_comm_startup(void){
   }
 
   ADCSRB = 0b01000000; // Enable analog comparator mux.
-  ACSR   = 0b00011000; // Enable analog comparator interrupt and clear ACI flag (analog comparator interrupt).
+  ACSR   = 0b00001000; // Enable analog comparator interrupt and clear ACI flag (analog comparator interrupt).
+
   PORTD &= 0b11110111; // Disable yellow LED (PD3).
   PORTD |= 0b00010000; // Enable green LED (PD4).
 }
@@ -89,10 +88,13 @@ void bldc_comm_shutdown(void){
 
   PORTB &= 0b11110001; // Disable HI_A (PB3), HI_B (PB2) and HI_C (PB1).
   PORTC &= 0b11111000; // Disable LI_A (PC2), LI_B (PC1) and LI_C (PC0).
+
   TCCR1A = 0b00000000; // Disable HI_C (0C1A) and HI_B (OC1B).
   TCCR2A = 0b10000001; // Enable HI_A (OC2A).
+
   ADCSRB = 0b00000000; // Disable analog comparator mux.
   ACSR   = 0b00010000; // Disable analog comparator interrupt and clear analog comparator interrupt flag.
+
   PORTD &= 0b11101111; // Disable green LED (PD4).
   PORTD |= 0b00001000; // Enable yellow LED (PD3).
 }
@@ -102,48 +104,60 @@ void bldc_comm_state(void){ // BLDC motor commutation function.
     case 0:
       PORTC &= 0b11111010; // Disable LI_A (PC2) and LI_C (PC0).
       PORTC |= 0b00000010; // Enable LI_B (PC1).
+
       TCCR1A = 0;          // Disable HI_C (0C1A) and HI_B (OC1B).
       TCCR2A = 0b10000001; // Enable HI_A (OC2A) in PWM, phase correct, 8-bit mode.
+
       ADMUX  = 0b00000101; // Select ADC5 (PC5) as comparator negative input.
       ACSR  |= 0b00000011; // Set interrupt on rising edge and enable comparator interrupt.
       break;
     case 1:
       PORTC &= 0b11111001; // Disable LI_A (PC2) and LI_B (PC1).
       PORTC |= 0b00000001; // Enable LI_C (PC0).
+
       TCCR1A = 0;          // Disable HI_C (0C1A) and HI_B (OC1B).
       TCCR2A = 0b10000001; // Enable HI_A (OC2A) in PWM, phase correct, 8-bit mode.
+
       ADMUX  = 0b00000100; // Select ADC4 (PC4) as comparator negative input.
       ACSR  &= 0b11111110; // Set interrupt on falling edge.
       break;
     case 2:
       PORTC &= 0b11111001; // Disable LI_A (PC2) and LI_B (PC1).
       PORTC |= 0b00000001; // Enable LI_C (PC0).
+
       TCCR1A = 0b00100001; // Enable HI_B (OC1B) in PWM, phase correct, 8-bit mode and disable HI_C (0C1A).
       TCCR2A = 0;          // Disable HI_A (OC2A).
+
       ADMUX  = 0b00000101; // Select ADC5 (PC5) as comparator negative input.
       ACSR  |= 0b00000011; // Set interrupt on rising edge.
       break;
     case 3:
       PORTC &= 0b11111100; // Disable LI_B (PC1) and LI_C (PC0).
       PORTC |= 0b00000100; // Enable LI_A (PC2).
+
       TCCR1A = 0b00100001; // Enable HI_B (OC1B) in PWM, phase correct, 8-bit mode and disable HI_C (0C1A).
       TCCR2A = 0;          // Disable HI_A (OC2A).
+
       ADMUX  = 0b00000011; // Select ADC3 (PC3) as comparator negative input.
       ACSR  &= 0b11111110; // Set interrupt on falling edge.
       break;
     case 4:
       PORTC &= 0b11111100; // Disable LI_B (PC1) and LI_C (PC0).
       PORTC |= 0b00000100; // Enable LI_A (PC2).
+
       TCCR1A = 0b10000001; // Enable HI_C (0C1A) in PWM, phase correct, 8-bit mode and disable HI_B (OC1B).
       TCCR2A = 0;          // Disable HI_A (OC2A).
+
       ADMUX  = 0b00000100; // Select ADC4 (PC4) as comparator negative input.
       ACSR  |= 0b00000011; // Set interrupt on rising edge.
       break;
     case 5:
       PORTC &= 0b11111010; // Disable LI_A (PC2) and LI_C (PC0).
       PORTC |= 0b00000010; // Enable LI_B (PC1).
+
       TCCR1A = 0b10000001; // Enable HI_C (0C1A) in PWM, phase correct, 8-bit mode and disable HI_B (OC1B).
       TCCR2A = 0;          // Disable HI_A (OC2A).
+      
       ADMUX  = 0b00000101; // Select ADC5 (PC5) as comparator negative input.
       ACSR  &= 0b11111110; // Set interrupt on falling edge.
       break;
@@ -160,7 +174,7 @@ ISR (ANALOG_COMP_vect) {
     if(bldc_state & 1){         // If BLDC state is odd (001, 011 or 101).
       if(!(ACSR & 0b00100000)){ // If 0 detected on ACO (comparator output), delay debouncing.
         i -= 1;
-        }
+      }
     }
     else{                       // If BLDC state is even (000, 010 or 100).
       if((ACSR & 0b00100000)){  // If 1 detected on ACO (comparator output), delay debouncing.
