f4838cf411496ab7e065145a9bd3114c5104199a
Aria Tso <ariatsoaria@gmail.com>
2024-05-06 21:24:48 +0100

    Added serial control to code. Separated functions to different files.

---
+++ BLDC_Software/UART_Comms.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "Arduino.h"
+#include "UART_Comms.h"
+
+bool UART_process_data(char * in_buffer, size_t in_buffer_size){
+  static unsigned int buffer_pos = 0;         // Buffer pointer variable.
+  unsigned char in_byte = Serial.read();      // Read in next byte in serial buffer.
+  switch (in_byte){
+    case '\n':                                // Detects end of text.
+      if (buffer_pos == 0){                   // If no text was entered, keep previous output.
+        return 1;
+      }
+      in_buffer[buffer_pos] = 0;              // Terminates string.
+      buffer_pos = 0;                         // Reset buffer location.
+      return 0;
+    case '\r':                                // Discard carriage return.
+      break;
+    default:
+      if (buffer_pos < (in_buffer_size - 1)){ // Only appends to string if buffer is not full.
+        in_buffer[buffer_pos++] = in_byte;    // Appends new byte to buffer and advances buffer pointer.
+      }
+      break;
+  }
+  return 1;
+}
---
+++ BLDC_Software/UART_Comms.h
@@ -0,0 +1,4 @@
+#define BAUD              9600
+#define UART_BUFFER_SIZE  64
+
+bool UART_process_data(char *, size_t);
