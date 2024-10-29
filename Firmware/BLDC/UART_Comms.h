e3d0f21e89f585505c61f4cf19185732fe068f81
Ariamelon <ariatsoaria@gmail.com>
2024-10-29 11:49:33 +0000

    Added license for software. Specified which license applies to which.

--- Firmware/BLDC/UART_Comms.h
+++ Firmware/BLDC/UART_Comms.h
@@ -1,3 +1,18 @@
+/*
+This file is part of BLDC control firmware.
+
+BLDC control firmware is free software: you can redistribute it and/or modify it
+under the terms of the GNUGeneral Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+BLDC control firmware is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with BLDC control firmware.
+If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 #define BAUD              9600
 #define UART_BUFFER_SIZE  64
 
--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 A brushless DC motor controller developed in partial fulfilment of the requirements for the degree of BEng (Hons) Electronic & Computer Engineering at the University of Brighton.
 
 ## Disclaimer
-This board is licensed under CERN-OHL-S-2.0.
+The hardware is licensed under CERN-OHL-S-2.0, and the firmware is licensed under GNU GPLv3. 
 
 ![Image 1](Documentation/Image-1.jpeg)
 ![Image 2](Documentation/Image-2.jpeg)
 
-
 https://github.com/user-attachments/assets/5595b5e6-1121-481e-8a8e-658866d14e04
-
